574ae83176093a38bcbd563b60fa34a9af834000
Carbuino <19510288+Carbuino@users.noreply.github.com>
2024-08-07 18:48:57 -0600

    Changed Removed API functions and removed Command menu

--- src/main/java/ca/carbuino/extension/emotemenu/utils/EmoteMenuUtils.java
+++ src/main/java/ca/carbuino/extension/emotemenu/utils/EmoteMenuUtils.java
@@ -1,7 +1,6 @@
 package ca.carbuino.extension.emotemenu.utils;
 
 import org.geysermc.cumulus.form.SimpleForm;
-import org.geysermc.cumulus.form.CustomForm;
 import org.geysermc.cumulus.util.FormImage;
 
 import org.geysermc.geyser.api.util.PlatformType;
@@ -22,7 +21,6 @@ public class EmoteMenuUtils {
             .button("Toggle Advanced Tooltips", FormImage.Type.PATH, "textures/ui/icon_recipe_equipment.png")
             .button("Advancements", FormImage.Type.PATH, "textures/ui/village_hero_effect.png")
             .button("Statistics", FormImage.Type.PATH, "textures/items/iron_pickaxe.png")
-            .button("Execute Command", FormImage.Type.PATH, "textures/blocks/command_block.png")
             .button("Geyser Settings", FormImage.Type.PATH, "textures/ui/settings_glyph_color_2x.png");
 
         builder.closedResultHandler(() -> {
@@ -32,41 +30,35 @@ public class EmoteMenuUtils {
                 case 1:
                     // Toggle Advanced Tooltips
                     if (checkSessionPermissions(session, "geyser.command.tooltips") == true) {
-                        session.getGeyser().commandManager().runCommand(session, "geyser tooltips");
+                        session.getGeyser().commandRegistry().runCommand(session, "geyser tooltips");
                     }
                     break;
 
                 case 2:
                     // View Advancements
                     if (checkSessionPermissions(session, "geyser.command.advancements") == true) {
-                        session.getGeyser().commandManager().runCommand(session, "geyser advancements");
+                        session.getGeyser().commandRegistry().runCommand(session, "geyser advancements");
                     }
                     break;
 
                 case 3:
                     // View Statistics
                     if (checkSessionPermissions(session, "geyser.command.statistics") == true) {
-                        session.getGeyser().commandManager().runCommand(session, "geyser statistics");
+                        session.getGeyser().commandRegistry().runCommand(session, "geyser statistics");
                     }
                     break;
 
                 case 4:
-                    // Execute Command Menu
-                    session.sendForm(EmoteMenuUtils.buildCommandForm(session));
-                    break;
-
-                    
-                case 5:
                     // View Geyser Settings
                     if (checkSessionPermissions(session, "geyser.command.settings") == true) {
-                        session.getGeyser().commandManager().runCommand(session, "geyser settings");
+                        session.getGeyser().commandRegistry().runCommand(session, "geyser settings");
                     }
                     break;
                     
                 default:
                     // Swap offhand
                     if (checkSessionPermissions(session, "geyser.command.offhand") == true) {
-                        session.getGeyser().commandManager().runCommand(session, "geyser offhand");
+                        session.requestOffhandSwap();
                     }
                     break;
 
@@ -88,26 +80,5 @@ public class EmoteMenuUtils {
         };
         session.sendMessage(ChatColor.RED + GeyserLocale.getPlayerLocaleString("geyser.bootstrap.command.permission_fail", session.locale()));
         return false;
-    };
-
-    /**
-    * Build the form for command menu option for the given session
-    *
-    * @param session The session to build the form for
-    */
-    private static CustomForm buildCommandForm(GeyserSession session) {
-        CustomForm.Builder builder = CustomForm.builder()
-            .title("Emote Menu - Execute Command")
-            .input("Command", "Enter a command");
-
-        builder.closedResultHandler(() -> {
-            session.sendForm(EmoteMenuUtils.buildEmoteForm(session));
-            
-        }).validResultHandler((response) -> {
-            session.sendCommand(response.asInput(0));
-            
-        });
-
-        return builder.build();
     };
 };
